405816abe1ed538d9b876815f10acc3c6adf7134
Tik88 <xtikx@yandex.ru>
2022-01-31 12:34:23 +0300

    Добавьте файлы проекта.

---
+++ RevitAPI_Lab3.3/PipeFilter.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI.Selection;
+
+namespace RevitAPI_Lab3._3
+{
+    class PipeFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Pipe;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
